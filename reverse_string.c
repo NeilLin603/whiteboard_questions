8c2089d24058e146348f3cb58cd91c9f7590685b
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-04-18 14:30:23 +0800

    Add files via upload

---
+++ reverse_string.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+#define SWAP(a, b) do { \
+    (a) ^= (b); \
+    (b) ^= (a); \
+    (a) ^= (b); \
+} while (0)
+
+int strLen(char *s) {
+    char *t = s - 1;
+    while (*++t);
+    return t - s;
+}
+
+void reverseString(char *s) {
+    int i = -1, j = strLen(s);
+    while (++i < --j) {
+        SWAP(s[i], s[j]);
+    }
+}
+
+int main() {
+    char s[] = "This is a string";
+    printf("Original string: %s\n", s);
+    reverseString(s);
+    printf("Reversed string: %s\n", s);
+
+    return 0;
+}
