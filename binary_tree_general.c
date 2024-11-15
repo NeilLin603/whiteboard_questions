84814ff4af6fb752bde4f13a6dd64df7ef05778e
NeilLin603 <160445926+NeilLin603@users.noreply.github.com>
2024-11-15 17:37:39 +0800

    Update binary_tree_general.c

--- binary_tree_general.c
+++ binary_tree_general.c
@@ -1,126 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define NULL_NODE -1
-
+/**
+ * Definition of a tree node.
+ */
 typedef struct TreeNode {
     int val;
     struct TreeNode *left, *right;
 } Node_t;
 
+/**
+ * Definition of a queue node.
+ */
 typedef struct QueueNode {
     Node_t *node;
     struct QueueNode *next;
 } QNode_t;
 
+/**
+ * Definition of a queue.
+ */
 typedef struct {
-    QNode_t *head;
-    QNode_t *tail;
+    QNode_t *head, *tail;
 } Queue_t;
 
-int enqueue(Queue_t *q, Node_t *node) {
-    QNode_t *new = (QNode_t *)malloc(sizeof(QNode_t));
-    if (!new) {
-        return 0;
-    }
-    new->node = node;
-    new->next = NULL;
-    if (q->head) {
-        q->tail->next = new;
-        q->tail = new;
-    } else {
-        q->head = q->tail = new;
-    }
-    return 1;
+/**
+ * \note Caller must call free() after use.
+ */
+Queue_t *initQueue() {
+    Queue_t *q = (Queue_t *)malloc(sizeof(Queue_t));
+    q->head = q->tail = NULL;
+    return q;
+}
+
+void enqueue(Queue_t *q, Node_t *node) {
+    QNode_t *tail = (QNode_t *)malloc(sizeof(QNode_t));
+    tail->node = node;
+    tail->next = NULL;
+    q->tail = q->tail ? (q->tail->next = tail) : (q->head = tail);
 }
 
 int dequeue(Queue_t *q, Node_t **node) {
     if (!q->head) {
         return 0;
     }
-    QNode_t *ret = q->head;
-    *node = ret->node;
-    q->head = ret->next;
-    free(ret);
-    if (!q->head) {
-        q->tail = NULL;
-    }
+    QNode_t *tmp = q->head;
+    *node = tmp->node;
+    q->head = tmp->next ? tmp->next : (q->tail = NULL);
+    free(tmp);
     return 1;
 }
 
+#define NULL_NODE -1
+
+#define ADD_NODE_MACRO(q, nums, node) if (*++nums != NULL_NODE) {              \
+    node = (Node_t *)malloc(sizeof(Node_t));                                   \
+    node->val = *nums;                                                         \
+    enqueue(q, node);                                                          \
+} else {                                                                       \
+    node = NULL;                                                               \
+}
+
 /**
- * \brief Build tree by array
+ * \brief Build tree by array.
  */
 Node_t *buildTree(int *nums, int numsSize) {
     if (!numsSize) {
         return NULL;
     }
+    Queue_t *q = initQueue();
     Node_t *root = (Node_t *)malloc(sizeof(Node_t)), *node;
     root->val = *nums;
-    Queue_t q = {NULL, NULL};
-    enqueue(&q, root);
-    while (dequeue(&q, &node)) {
-        if (node) {
-            if (--numsSize > 0) {
-                if (*++nums != NULL_NODE) {
-                    node->left = (Node_t *)malloc(sizeof(Node_t));
-                    node->left->val = *nums;
-                } else {
-                    node->left = NULL;
-                }
-                enqueue(&q, node->left);
-            } else {
-                node->left = NULL;
-            }
-            if (--numsSize > 0) {
-                if (*++nums != NULL_NODE) {
-                    node->right = (Node_t *)malloc(sizeof(Node_t));
-                    node->right->val = *nums;
-                } else {
-                    node->right = NULL;
-                }
-                enqueue(&q, node->right);
-            } else {
-                node->right = NULL;
-            }
-        } else {
-            numsSize -= 2;
+    enqueue(q, root);
+    while (--numsSize) {
+        dequeue(q, &node);
+        ADD_NODE_MACRO(q, nums, node->left);
+        if (!--numsSize) {
+            node->right = NULL;
+            break;
         }
+        ADD_NODE_MACRO(q, nums, node->right);
     }
+    while (dequeue(q, &node)) {
+        node->left = node->right = NULL;
+    }
+    free(q);
     return root;
 }
 
 /**
- * \brief Print tree
+ * \brief Print tree.
  */
-void printTree(Node_t *root, char *name) {
-    printf("%s = [", name);
-    Queue_t q = {NULL, NULL};
-    enqueue(&q, root);
-    Node_t *node;
-    while (dequeue(&q, &node)) {
-        if (node) {
-            printf("%d,", node->val);
-            enqueue(&q, node->left);
-            enqueue(&q, node->right);
-        } else {
-            printf("null,");
+void printTree(Node_t *root, char *title) {
+    printf("%s = [", title);
+    if (root) {
+        printf("%d", root->val);
+        Queue_t *q = initQueue();
+        enqueue(q, root->left);
+        enqueue(q, root->right);
+        int nullCnt = 0;
+        while (dequeue(q, &root)) {
+            if (!root) {
+                nullCnt++;
+                continue;
+            }
+            while (nullCnt) {
+                printf(",null");
+                nullCnt--;
+            }
+            printf(",%d", root->val);
+            enqueue(q, root->left);
+            enqueue(q, root->right);
         }
+        free(q);
     }
-    printf("\b]\n");
+    printf("]\n");
 }
 
-void freeTree(Node_t **root) {
-    if (*root) {
-        freeTree(&(*root)->left);
-        freeTree(&(*root)->right);
-        free(*root);
-        *root = NULL;
+void freeTree(Node_t *root) {
+    if (root) {
+        freeTree(root->left);
+        freeTree(root->right);
+        free(root);
     }
 }
 
 /**
- * \brief Invert tree
+ * \brief Invert tree.
  */
 void invertTree(Node_t *root) {
     if (root) {        
@@ -147,7 +153,7 @@ static int isSymmetricSub(Node_t *left, Node_t *right) {
 }
 
 /**
- * \brief Check whether tree is symmetric
+ * \brief Check whether tree is symmetric.
  */
 int isSymmetric(Node_t *root) {
     return root ? isSymmetricSub(root->left, root->right) : 1;
@@ -179,7 +185,7 @@ int main() {
         printf("This tree is %ssymmetric.\n\n", s);
 
         // Free tree
-        freeTree(&root);
+        freeTree(root);
     }
 
     return 0;
